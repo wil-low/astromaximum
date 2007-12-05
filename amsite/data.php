9a85cec3dcc0590c9f122e90502695e87036e9b9
willow <willow@localhost>
2007-12-05 21:11:11 +0200

    data.php added

---
+++ amsite/data.php
@@ -0,0 +1,39 @@
+<?php
+//include_once('lang.php');
+include_once('dbconnect.php');
+if(true /*|| check_access()*/){
+	if(isset($_GET['r'])){
+		$type='r';
+	}
+	if(isset($_GET['d'])){
+		$type='d';
+	}
+	if(isset($_GET['t'])){
+		$type='t';
+	}
+	$dig=$_GET[$type];
+	$idd=substr($dig, -4);
+	$ye=substr($dig, 0,2);
+	$fn="$DIR_FILES/".$dig.".$type";
+	$stat=sprintf(
+		"UPDATE files SET used='t' WHERE id=%s AND type='%s'", $dig, $type);
+	mysql_query($stat);
+	if(strcmp($type,'t')==0){
+		$type='d';
+	}
+
+	$handle = fopen($fn, "rb");
+	$data = fread($handle, filesize($fn));
+	fclose($handle);
+	if(strcmp($type,'d')==0){
+		header('Content-type: text/vnd.sun.j2me.app-descriptor');
+	}
+	else{
+//		echo filesize($fn); 
+		header('Content-type: application/java-archive');
+	}
+	header('Content-Disposition: attachment; filename="Cities'."'$ye-".$idd.'.ja'.$type.'"', false);
+	echo $data;
+
+}
+?>
