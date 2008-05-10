95f91ff3521088f560a7d7988c36f718daa93939
willow <willow@localhost>
2008-05-10 16:10:24 +0300

    added Profiling Makefile

---
+++ Astromaximum/mutter2/nbproject/Makefile-Profiling.mk
@@ -0,0 +1,72 @@
+#
+# Gererated Makefile - do not edit!
+#
+# Edit the Makefile in the project folder instead (../Makefile). Each target
+# has a -pre and a -post target defined where you can add customized code.
+#
+# This makefile implements configuration specific macros and targets.
+
+
+# Environment
+MKDIR=mkdir
+CP=cp
+CCADMIN=CCadmin
+RANLIB=ranlib
+CC=gcc
+CCC=g++
+CXX=g++
+FC=
+
+# Include project Makefile
+include Makefile
+
+# Object Directory
+OBJECTDIR=build/Profiling/GNU-Linux-x86
+
+# Object Files
+OBJECTFILES= \
+	${OBJECTDIR}/evclass.o \
+	${OBJECTDIR}/datafile.o \
+	${OBJECTDIR}/main.o
+
+# C Compiler Flags
+CFLAGS=
+
+# CC Compiler Flags
+CCFLAGS=-pg
+CXXFLAGS=-pg
+
+# Fortran Compiler Flags
+FFLAGS=
+
+# Link Libraries and Options
+LDLIBSOPTIONS=-L../swe -lswe
+
+# Build Targets
+.build-conf: ${BUILD_SUBPROJECTS} mutter2prof
+
+mutter2prof: ${OBJECTFILES}
+	${LINK.cc} -pg -o mutter2prof ${OBJECTFILES} ${LDLIBSOPTIONS} 
+
+${OBJECTDIR}/evclass.o: evclass.cpp 
+	${MKDIR} -p ${OBJECTDIR}
+	$(COMPILE.cc) -g -DANSITZ -I../swe -o ${OBJECTDIR}/evclass.o evclass.cpp
+
+${OBJECTDIR}/datafile.o: datafile.cpp 
+	${MKDIR} -p ${OBJECTDIR}
+	$(COMPILE.cc) -g -DANSITZ -I../swe -o ${OBJECTDIR}/datafile.o datafile.cpp
+
+${OBJECTDIR}/main.o: main.cpp 
+	${MKDIR} -p ${OBJECTDIR}
+	$(COMPILE.cc) -g -DANSITZ -I../swe -o ${OBJECTDIR}/main.o main.cpp
+
+# Subprojects
+.build-subprojects:
+
+# Clean Targets
+.clean-conf:
+	${RM} -r build/Profiling
+	${RM} mutter2prof
+
+# Subprojects
+.clean-subprojects:
