461ca3b1e1b12e164591e2a8fa97dddcbe46a2af
willow <willow@localhost>
2009-01-04 18:46:28 +0200

    paypal.php uses paypal.class.php

--- Astromaximum/site/mobi/html/paypal.php
+++ Astromaximum/site/mobi/html/paypal.php
@@ -1,9 +1,15 @@
 <?php
 if(!isset($EXEC)) die("Access restricted");
+
+if (empty($_GET['action'])){
+	$_GET['action']='';
+	if($chac != 3){
+		die ('Access restricted');
+	}
+}  
+
 $cur_year=$GLOBALS['amax']['year'];
 $price=$GLOBALS['amax']['price'];
-
-$admin_email='dev@astromaximum.mobi';
 
 $paypal_email='aivush_1217502939_biz@gmail.com';//$GLOBALS['amax']['mail_office'];
 
@@ -17,9 +23,75 @@ print_r ($_POST);
 echo "</pre>";
 echo "<br/>$item_name, $item_price, $currency <br/>";
 */
-
 $thisurl='http://'.$_SERVER['SERVER_NAME']."/?$lang_&p=paypal";
 
+require_once('mobi/paypal/paypal.class.php');  // include the class file
+$p = new paypal_class;             // initiate an instance of the class
+$p->paypal_url = 'https://www.sandbox.paypal.com/cgi-bin/webscr';   // testing paypal url
+//$p->paypal_url = 'https://www.paypal.com/cgi-bin/webscr';     // paypal url
+
+switch ($_GET['action']) {
+
+   case 'success':      // Order was successful...
+   
+      // This is where you would probably want to thank the user for their order
+      // or what have you.  The order information at this point is in POST 
+      // variables.  However, you don't want to "process" the order until you
+      // get validation from the IPN.  That's where you would have the code to
+      // email an admin, update the database with payment status, activate a
+      // membership, etc.  
+ 
+      echo "<h3>Thank you for your order.</h3>";
+      foreach ($_POST as $key => $value) { echo "$key: $value<br>"; }
+      
+      // You could also simply re-direct them to another page, or your own 
+      // order status page which presents the user with the status of their
+      // order based on a database (which can be modified with the IPN code 
+      // below).
+      
+      return;
+      
+   case 'cancel':       // Order was canceled...
+
+      // The order was canceled before being completed.
+ 
+      echo "The order was canceled.";
+      
+      return;
+      
+   case 'ipn':          // Paypal is calling page for IPN validation...
+   
+      // It's important to remember that paypal calling this script.  There
+      // is no output here.  This is where you validate the IPN data and if it's
+      // valid, update your database to signify that the user has payed.  If
+      // you try and use an echo or printf function here it's not going to do you
+      // a bit of good.  This is on the "backend".  That is why, by default, the
+      // class logs all IPN data to a text file.
+      
+      if ($p->validate_ipn()) {
+          
+         // Payment has been recieved and IPN is verified.  This is where you
+         // update your database to activate or process the order, or setup
+         // the database with the user's order details, email an administrator,
+         // etc.  You can access a slew of information via the ipn_data() array.
+  
+         // Check the paypal documentation for specifics on what information
+         // is available in the IPN POST variables.  Basically, all the POST vars
+         // which paypal sends, which we send back for validation, are now stored
+         // in the ipn_data() array.
+  
+         // For this example, we'll just email ourselves ALL the data.
+         $subject = 'Recieved IPN Payment';
+         $body =  "An instant payment notification was successfully recieved\n";
+         $body .= "from ".$p->ipn_data['payer_email']." on ".date('m/d/Y');
+         $body .= " at ".date('g:i A')."\n\nDetails:\n";
+         
+         foreach ($p->ipn_data as $key => $value) { $body .= "\n$key: $value"; }
+         event_send($subject, $body);
+      }
+      return;
+ }     
+/*
 if(isset($_GET['mode']) && ($_GET['mode'] =='success')){ // successful payment
 
 //  validate trasaction
@@ -101,7 +173,7 @@ if(isset($_GET['mode']) && ($_GET['mode'] == 'cancel')){ // cancelled
     echo "Payment cancelled";
     return;    
 }
-
+*/
 echo <<< EOF
 <h4 style="font-size:12px;">1. Оплатить через PayPal:</h4>
 <form method="post" action= "https://www.sandbox.paypal.com/cgi-bin/webscr">
@@ -112,19 +184,21 @@ echo <<< EOF
 <input type="hidden" name="amount" value="$item_price"/>
 <input type="hidden" name="currency_code" value="$currency"/>
 <input type="hidden" name="no_shipping" value="1"/>
-<input type="hidden" name="notify_url" value="{$thisurl}"/>
-<input type="hidden" name="return" value="{$thisurl}&mode=success"/>
+<input type="hidden" name="notify_url" value="{$thisurl}&action=ipn"/>
+<input type="hidden" name="return" value="{$thisurl}&action=success"/>
 <input type="hidden" name="rm" value="2"/>
-<input type="hidden" name="cancel_return" value="{$thisurl}&mode=cancel"/>
-<input type="image" src="https://www.sandbox.paypal.com/en_US/i/btn/btn_buynowCC_LG.gif" width="122" height="47" border="0" name="submit" alt="PayPal - The safer, easier way to pay online!">
+<input type="hidden" name="cancel_return" value="{$thisurl}&action=cancel"/>
+<input type="image" src="https://www.sandbox.paypal.com/en_US/i/btn/btn_buynowCC_LG.gif" 
+	width="122" height="47" border="0" name="submit" 
+	alt="PayPal - The safer, easier way to pay online!"/>
 </form>
 EOF;
 
 function qsmart($post_key)
 {
-    if(!isset($_POST[$post_key])) 
-	return "''";
-    else
-	return quote_smart($_POST[$post_key]);
+	if(!isset($_POST[$post_key])) 
+		return "''";
+	else
+		return quote_smart($_POST[$post_key]);
 }
 ?>
