c585e073820b11cac6849c3f38848d58432899c4
vmisyats <vmisyats@localhost>
2009-08-08 15:47:05 +0300

    English home page added

---
+++ Astromaximum/site/mobi/html/en/home.php
@@ -0,0 +1,43 @@
+<?php 
+if(!isset($EXEC)) die("Access restricted");
+$META_TITLE="Astrological calendar";
+$META_DESCR="The most complete astrological calendar for mobile devices: phones, smartphones, PDAs etc. Accessible interface facilitates access to astrological information for professionals and ordinary customers. A wide range of astrological tools allows you to choose the best time for your business.";
+$META_KEYWORDS='astrology, numerology, arithomancy, arithmancy, amulet, talisman, planets, stars, astromancy, prognosis, horoscope'; 
+?>
+<p>
+<b>ASTROMAXIMUM </b>is the most detailed astrological calendar, and at the same time,<br /> 
+<b>ASTROMAXIMUM</b> is the first astrological calendar for mobile phones.
+</p>
+
+<p>
+Are you going to contact your supervisor to request a salary increase but do not want to come to the humiliating rejection? Do you want to start your own successful business? Are you doing alright and plan to buy a car, but do not want to become a regular client of a car service? Are you going to do a medical examination or surgery? Are you about to open a bank account?<br />
+<b>ASTROMAXIMUM</b> - this is the most convenient tool to find the best time to implement your plans and aspirations.
+</p>
+
+<p>
+Choose an evil of the lesser kind, do not miss the best benefit, estimate equivalent options, recognize scraps, find the best moment for global projects as well as for small cases - only your intuition could be an assistant, and if it goes wrong, <b>ASTROMAXIMUM</b> will help you.
+</p>
+
+<p><b>ASTROMAXIMUM</b> displays the following events*:</p>
+<ul>
+<li> unfavorable position of the Moon ('void-of-course', VOC) and the passage of the Moon over &lt;burned road&gt; ('via combusta', VC)</li>
+<li> Solar day number, according to seasonal Persian calendar Fasli</li>
+<li> sign of the Zodiac and the degree of the Sun</li>
+<li> astronomical sunrise and sunset at given location</li>
+<li> Moon phase</li>
+<li> Lunar day number</li>
+<li> sign of the Zodiac of the Moon</li>
+<li> astronomical moonrise and moonset at given location</li>
+<li> number of lunar Tithi days - days of the lunar month of Vedic astrology</li>
+<li> aspects of the Moon and Moon passage between aspects</li>
+<li> important degrees of planets; aspects of planets, including Uranus, Neptune and Pluto</li>
+<li> planetary hours</li>
+<li> degrees of Mercury, Venus, Mars, Jupiter, Saturn, North Node, Black and White Moon</li>
+<li> Septenary planets on horoscope axes</li>
+<li> retrograde motion time ranges</li>
+<li> Zodiac signs ingression</li>
+<li> ASTROMAXIMUM provides a rapid horoscope for any event in the past since 1971</li>
+<li> decumbiture - ASTROMAXIMUM predicts the course of the disease</li>
+<li> time of solar and lunar eclipses.</li>
+</ul> 
+<p>* time range is given; each event is accompanied by a detailed astrological comment</p>
