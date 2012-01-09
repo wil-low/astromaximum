247ace4171d26f264bf5e72b095dd5f372d4403f
willow <willow@localhost>
2012-01-09 18:27:12 +0200

    Date navigation

--- Astromaximum/site2/summary/views.py
+++ Astromaximum/site2/summary/views.py
@@ -1,11 +1,18 @@
 from django.template import RequestContext
 from django.shortcuts import render_to_response
-from datetime import *
+import datetime
 
-def main(request, datestr=None):
+def main(request, year, month, day):
     "prints home page"
-    if datestr is None:
-        datestr = 'today'
-    c = RequestContext(request, {'date': datestr})
+    date = datetime.datetime(int(year), int(month), int(day))
+    current_date = date.strftime("%Y-%m-%d")
+    prev_date = (date + datetime.timedelta(days=-1)).strftime("%Y-%m-%d")
+    next_date = (date + datetime.timedelta(days=1)).strftime("%Y-%m-%d")
+    params = {
+              'current_date': current_date,
+              'prev_date': prev_date,
+              'next_date': next_date,
+              }
+    c = RequestContext(request, params)
     #raise Exception(c)
     return render_to_response('summary.html', context_instance=c)
--- Astromaximum/site2/urls.py
+++ Astromaximum/site2/urls.py
@@ -3,21 +3,17 @@ from django.conf import settings
 from django.contrib import admin
 admin.autodiscover()
 
-urlpatterns = patterns('',
-    url(r'^$', 'summary.views.main'),
-
-    url(r'^common/gen/(?P<year>\d{4})/$', 'amax.views.common_generate'),
-    url(r'^common/show/$', 'amax.views.common_show'),
-    url(r'^common/show/(?P<year>\d{4})-(?P<month>\d{2})-(?P<day>\d{2})/$', 'amax.views.common_show'),
-    url(r'^common/clean/(?P<year>\d{4})/$', 'amax.views.common_clean'),
-
-    url(r'^gen/(?P<year>\d{4})/(?P<region>\w+)/(?P<city_id>\w{8})/$', 'amax.views.location_generate'),
-
-    url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
-    url(r'^admin/', include(admin.site.urls)),
-)
+urlpatterns = []
 
 if settings.DEBUG:
     urlpatterns += patterns('',
         (r'^i/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.IMAGE_ROOT}),
     )
+
+urlpatterns += patterns('',
+    url(r'^m/', include('amax.urls')),
+
+    url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+    url(r'^admin/', include(admin.site.urls)),
+)
+
