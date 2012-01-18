c8d6ca70104de782b2c7b3fef5124e2ddbe471b6
willow <willow@localhost>
2012-01-18 17:26:13 +0200

    Deployment script added

---
+++ Astromaximum/site2/scripts/make_archive.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+set -x
+DATE=`date +%Y%m%d`
+TMPDIR=tmp_deploy/
+FILES='*.py amax/*.py desktop/*.py i/*.css i/*.png m/*.py m/templatetags/*.py mobi/*.py public/ templates/'
+PROJECT_ROOT=$1
+
+rm -rf $TMPDIR
+mkdir -p $TMPDIR deploy/
+cp -r --parents $FILES $TMPDIR
+cd $TMPDIR
+
+# change files
+mkdir -p data/{commons,locations}
+chmod +x public/django.fcgi
+
+perl -lape "s%PROJECT_ROOT = .+%PROJECT_ROOT = '$PROJECT_ROOT'%" settings.py > settings.tmp
+mv settings.tmp settings.py
+
+TARGETFILE=../deploy/amdj_${DATE}.tgz
+rm -f $TARGETFILE
+tar czfv $TARGETFILE .
