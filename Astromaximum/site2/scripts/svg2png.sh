3d787ff03026797e6fac31050d75e30a2e546bb2
willow <willow@localhost>
2012-01-18 17:33:44 +0200

    svg2png fixes

--- Astromaximum/site2/scripts/svg2png.sh
+++ Astromaximum/site2/scripts/svg2png.sh
@@ -1,9 +1,11 @@
+#!/bin/bash
 #set -x
-mkdir -p ../i/
-cd art
+WORKDIR=`pwd`
+mkdir -p i/
+cd src/art
 for i in *.svg; do 
     fname=${i%.svg};
-    perl ../strip_flow_root.pl < $i | convert - ../../i/$fname.png;
+    perl $WORKDIR/scripts/strip_flow_root.pl < $i | convert - $WORKDIR/i/$fname.png;
     echo $fname; 
 done
 
